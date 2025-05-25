c2613d384352f5a1e994da3979af8251064678ff
dineshpatte <152471120+dineshpatte@users.noreply.github.com>
2025-05-25 21:34:58 +0530

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
-import navbar from './components/navbar'
+import Navbar from './components/navbar'
 import Projects from './pages/projects'
 import ProjectDetail from './pages/projectsDetail'
 import Clone from './pages/clone'
@@ -9,7 +9,7 @@ import Clone from './pages/clone'
 function App() {
   return (
     <div>
-      <navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
